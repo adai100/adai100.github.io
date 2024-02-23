c1bf9979a909f04cb7107146f3d3841937bf2f63
hbyhzx <2486251172@qq.com>
2024-02-23 09:25:42 +0800

    xiugai

--- check_share_id.py
+++ check_share_id.py
@@ -8,13 +8,33 @@ import requests
 import re, json
 import os
 
-# content = ""
-# subject = ""
-# err=''
+
+def get_list_by_share(share_id, parent_file_id, share_pwd=""):
+    if share_pwd == "wumima":
+        share_pwd = ""
+    share_token = requests.post(
+        url="https://api.aliyundrive.com/v2/share_link/get_share_token",
+        headers={"Content-Type": "application/json"},
+        json={"share_id": share_id, "share_pwd": share_pwd},
+    ).json()["share_token"]
+    url = "https://api.aliyundrive.com/adrive/v2/file/list_by_share"
+    headers = {"x-share-token": share_token}
+    json1 = {
+        "share_id": share_id,
+        "parent_file_id": parent_file_id,
+        "limit": 200,
+        "image_thumbnail_process": "image/resize,w_256/format,jpeg",
+        "image_url_process": "image/resize,w_1920/format,jpeg/interlace,1",
+        "video_thumbnail_process": "video/snapshot,t_1000,f_jpg,ar_auto,w_256",
+        "order_by": "name",
+        "order_direction": "ASC",
+    }
+    return (requests.post(url, headers=headers, json=json1).json())["items"]
+
+
 alishares = []
 # 检查alishare_list分享的有效性
 tempdir = os.path.join(os.path.dirname(__file__), "temp")
-# os.makedirs(tempdir)
 shutil.rmtree(tempdir)
 fname = os.path.join(os.path.dirname(__file__), "alishare_list.txt")
 outputtxtfname = os.path.join(os.path.dirname(__file__), "alisharelist.txt")
@@ -71,11 +91,15 @@ for row in alishares:
             }
             output_json.append(item)
             output_txt += f"{mount_path} {line[1]}\n"
-        if not os.path.exists(os.path.join(tempdir,mount_path)):
-            os.makedirs(os.path.join(tempdir,mount_path))
+        if not os.path.exists(os.path.join(tempdir, mount_path)):
+            os.makedirs(os.path.join(tempdir, mount_path))
+        json1 = get_list_by_share(
+            item["share_id"], item["parent_file_id"], item["share_pwd"]
+        )
         with open(os.path.join(tempdir, mount_path, "index.json"), "w") as f:
-            json.dump(item, f)
-        print(line[0].strip("/").replace("/", "|"))
+            json.dump(json1, f)
+
+        print(line[0])
     sleep(1)
 # with open(outputtxtfname, "w", encoding="utf-8") as f:
 #     f.write(output_txt)
